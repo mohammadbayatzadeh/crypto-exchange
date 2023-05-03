3468376486a9c2a9a975377461a49b5602ed71c9
mohammadbayatzadeh <mamadbayat777@gmail.com>
2023-05-03 19:15:53 +0430

    sparklines data added

--- src/components/Cryptos.js
+++ src/components/Cryptos.js
@@ -1,19 +1,46 @@
-import React from 'react';
+import React from "react";
 
 //styles
-import styles from "./Cryptos.module.css"
-const Cryptos = ({ name, symbol, image, price, marketCap, change }) => {
-    return (
-        <div className={styles.body}>
-            <div className={styles.container}>
-                <img className={styles.image} src={image} alt={name} />
-                <span className={styles.name}>{name}({symbol})</span>
-                <span className={styles.price}>$ {price.toLocaleString()}</span>
-                <span className={change > 0 ? styles.greenchange : styles.redchange}>% {change}</span>
-                <span className={styles.marketCap}>$ {marketCap.toLocaleString()}</span>
-            </div>
-        </div>
-    );
+import styles from "./Cryptos.module.css";
+
+//sparkline
+const sparkline = require("node-sparkline");
+
+const Cryptos = ({
+  name,
+  symbol,
+  image,
+  price,
+  marketCap,
+  change,
+  sparklines,
+}) => {
+  const values = [...sparklines];
+  let svg = sparkline({
+    values,
+    width: 150,
+    height: 50,
+    stroke: change > 0 ? "green" : "red",
+    strokeWidth: 1.25,
+    strokeOpacity: 1,
+  });
+
+  return (
+    <div className={styles.body}>
+      <div className={styles.container}>
+        <img className={styles.image} src={image} alt={name} />
+        <span className={styles.name}>
+          {name}({symbol})
+        </span>
+        <span className={styles.price}>$ {price.toLocaleString()}</span>
+        <span className={change > 0 ? styles.greenchange : styles.redchange}>
+          % {change}
+        </span>
+        <span className={styles.marketCap}>$ {marketCap.toLocaleString()}</span>
+        {svg && <div dangerouslySetInnerHTML={{ __html: svg }} />}
+      </div>
+    </div>
+  );
 };
 
 export default Cryptos;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,54 +1,64 @@
-import React, { useEffect, useState } from 'react';
-import { Getdata } from '../server/api';
+import React, { useEffect, useState } from "react";
+import { Getdata } from "../server/api";
 
 //comps
-import Cryptos from './Cryptos';
-import Loader from './Loader';
+import Cryptos from "./Cryptos";
+import Loader from "./Loader";
 
 //styles
-import styles from "./Home.module.css"
+import styles from "./Home.module.css";
 const Home = () => {
-    const [cryptos, setCryptos] = useState([]);
-    const [search, setSearch] = useState([]);
-    useEffect(() => {
+  const [cryptos, setCryptos] = useState([]);
+  const [search, setSearch] = useState([]);
+  useEffect(() => {
+    const fetchAPI = async () => {
+      const data = await Getdata();
+      console.log(data);
+      setCryptos(data);
+    };
 
-        const fetchAPI = async () => {
-            const data = await Getdata();
-            console.log(data);
-            setCryptos(data);
-        }
-
-        fetchAPI();
-    }, [])
-    const searchHandler = event => {
-        setSearch(event.target.value.toLowerCase())
-    }
-    const searchedCryptos = cryptos.filter(crypto => crypto.name.toLowerCase().includes(search))
-    return (
-        <div className={styles.container}>
-            <input className={styles.input} type="text" placeholder="Search..." value={search} onChange={searchHandler} />
-            {cryptos.length ?
-                <div className={styles.coinContainer} >
-                    {
-                        searchedCryptos.map(crypto =>
-                            <Cryptos
-                                key={crypto.id}
-                                name={crypto.name}
-                                symbol={crypto.symbol}
-                                image={crypto.image}
-                                price={crypto.current_price}
-                                marketCap={crypto.market_cap}
-                                change={crypto.price_change_percentage_24h}
-                            />)
-                    }
-                </div> :
-                <Loader />
-            }
-            {
-                searchedCryptos.length > 0 ? " " : <p className={styles.feiled}>Nothing Found !..</p>
-            }
+    fetchAPI();
+  }, []);
+  const searchHandler = (event) => {
+    setSearch(event.target.value.toLowerCase());
+  };
+  const searchedCryptos = cryptos.filter((crypto) =>
+    crypto.name.toLowerCase().includes(search)
+  );
+  return (
+    <div className={styles.container}>
+      <input
+        className={styles.input}
+        type="text"
+        placeholder="Search..."
+        value={search}
+        onChange={searchHandler}
+      />
+      {cryptos.length ? (
+        <div className={styles.coinContainer}>
+          {searchedCryptos.map((crypto) => (
+            <Cryptos
+              key={crypto.id}
+              name={crypto.name}
+              symbol={crypto.symbol}
+              image={crypto.image}
+              price={crypto.current_price}
+              marketCap={crypto.market_cap}
+              change={crypto.price_change_percentage_24h}
+              sparklines={crypto.sparkline_in_7d.price}
+            />
+          ))}
         </div>
-    );
+      ) : (
+        <Loader />
+      )}
+      {searchedCryptos.length > 0 ? (
+        " "
+      ) : (
+        <p className={styles.feiled}>Nothing Found !..</p>
+      )}
+    </div>
+  );
 };
 
 export default Home;
